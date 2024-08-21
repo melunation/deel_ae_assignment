41bf443df303e2710a23f8eba54031390a2fbed1
melunation <karlokas@hotmail.com>
2024-08-21 10:33:15 +0200

    updating ymls, adding keys and tests

--- models/datamart/transactions_cube.sql
+++ models/datamart/transactions_cube.sql
@@ -8,4 +8,8 @@ with transactions as (
     group by 1, 2
 )
 
-select * from transactions
+-- this is again something that in snowflake we could have done inside the cte above because it allows for alias refering
+select 
+    *,
+    {{dbt_utils.generate_surrogate_key(['date', 'country'])}} as unique_key
+from transactions
